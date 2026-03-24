3370736181a9e4a3f27f24f7db77b8acba56c966
Andres Rios <riosandres294@gmail.com>
2026-03-23 21:45:36 -0500

    several filters added to macroeconomics -> GDP section

--- pages/helpers/macro/macro_charts.py
+++ pages/helpers/macro/macro_charts.py
@@ -1,8 +1,8 @@
 import plotly.express as px
 import pandas as pd
 
-def total_gdp_line(data: pd.DataFrame):
-    fig = px.line(data, labels={"index": "Year", "value": "Billions (COP)"}, title="GDP per Year")
+def total_gdp_line(data: pd.DataFrame, labels: dict):
+    fig = px.line(data, labels={"index": "Year", "value": "Billions (COP)"}, title="GDP per Year" )
     fig.update_layout(
 
         height=600,
@@ -14,7 +14,7 @@ def total_gdp_line(data: pd.DataFrame):
             "xanchor": 'left'
         },
         
-        showlegend=False,
+        #showlegend=False,
         
         margin=dict(l=50, r=20, t=80, b=0),
     )
@@ -40,5 +40,22 @@ def total_gdp_line(data: pd.DataFrame):
         yaxis_title_font=dict(size=15),
     )
 
+    fig.for_each_trace(lambda t: t.update(name = labels.get(t.name, t.name), 
+                                          hovertemplate = "<b>%{fullData.name}</b><br>Year: %{x}<br>Billions (COP): %{y}<extra></extra>"))
+
+    fig.update_layout(legend_title_text="")
+
+    fig.update_layout(
+        legend=dict(
+            orientation="h",   
+            yanchor="bottom", 
+            y=1.02,            
+            xanchor="center", 
+            x=0.5,             
+            entrywidth=0,      
+            entrywidthmode="pixels"
+        ),
+        margin=dict(t=80)      
+    )
     return fig
 
--- pages/tabs/gdp.py
+++ pages/tabs/gdp.py
@@ -2,29 +2,78 @@ import streamlit as st
 import pandas as pd
 from pages.helpers.macro import macro_functions as mf
 from pages.helpers.macro import macro_charts as mc
+import translations.gdp_spend as t
+from translations.gdp_production import production_summarize_terms as p
 
-def render_gdp(gdp_df: pd.DataFrame) -> None:
-    gdp_local = gdp_df.copy()
-
-    with st.sidebar:
+def generalities(df: pd.DataFrame, terms: dict, variable: int|list) -> None:
+    """
+    General utilities used in several parts of this section.
+    filters in the sidebar and the logic to plot the chart
+    """
+    with st.sidebar: 
         st.header("Filters:")
-
-        years =  gdp_local.columns[1:].str.split("-").str[0].unique()
+        years =  df.columns[1:].str.split("-").str[0].unique()
         choice_year = st.multiselect("Year:", sorted(years, reverse=True))
+
+        tmp = st.multiselect("Variable:", terms.values())
+        if tmp:
+            variable = [k for k, v in terms.items() if v in tmp]
 
         st.info("You may choose more than one option.")
 
-    st.title("Real GDP")
-
     if choice_year:
         pattern = "|".join(choice_year)
-        mask = gdp_local.columns.str.contains(pattern)
+        mask = df.columns.str.contains(pattern)
         mask[0] = True
-        gdp_local = gdp_local.loc[:, mask]
+        df = df.loc[:, mask]
 
-    gdp_series = mf.clean_gdp(gdp_local)
-    fig = mc.total_gdp_line(gdp_series) 
+    # plot the chart
+    gdp_series = mf.clean_gdp(df, variable)
+    fig = mc.total_gdp_line(gdp_series, terms) 
     st.plotly_chart(fig)
     st.caption("Chained volume series, base year 2015")
     st.caption("Source: DANE")
     st.info("\'p\' is provisional and \'pr\' is preliminary data.")
+
+def render_gdp(gdp_df: pd.DataFrame) -> None:
+    gdp_local = gdp_df.copy()
+ 
+    st.title("Real GDP")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        method = st.selectbox("Method:", ["Total Values", "Annual Growth", "Quarter Growth"]) 
+
+    if method == "Total Values":
+        with col2:
+            perspective = st.selectbox("Perspective:", ["Spend", "Production"])
+
+        if perspective == "Spend":
+            with col3:
+                category = st.selectbox("Category:", t.spend_categories.values())
+
+            file = next((k for k, v in t.spend_categories.items() if v == category), None)
+            term_name = f"spend_{file}_terms"
+            selected_terms = getattr(t, term_name, None)
+
+            if category != "Summarize":
+                path = f"./data/dane/GDP/spend/{file}.csv"
+                gdp_local = pd.read_csv(path, encoding="utf-8", dtype=str)
+                variable = 0
+            else:
+                variable = -1
+
+            generalities(gdp_local, selected_terms, variable)
+        else:
+            path = "./data/dane/GDP/production/summarize.csv" 
+            gdp_local = pd.read_csv(path, encoding="utf-8", dtype=str)
+
+            with col3:
+                category = st.selectbox("Category:", ["Summarize"])
+                
+            variable = -1
+
+            generalities(gdp_local, p, variable)
+    else:
+        st.write("SOON")
