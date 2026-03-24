b1c421794ddcf878662d13cfc8f6ef739652a113
Andres Rios <riosandres294@gmail.com>
2026-03-23 21:45:36 -0500

    several filters added to macroeconomics -> GDP section

--- pages/helpers/macro/macro_functions.py
+++ pages/helpers/macro/macro_functions.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
-def clean_gdp(df: pd.DataFrame) -> pd.Series:
+def clean_gdp(df: pd.DataFrame, columns):
     gdp_local = df.copy()
 
     gdp_local = gdp_local.set_index("Concepto")
     gdp_local = gdp_local.apply(lambda col: col.str.replace(".", "", regex=False))
 
-    gdp_series = gdp_local.iloc[-1,:]
+    if isinstance(columns, int):
+        gdp_series = gdp_local.iloc[columns,:]
+    else:
+        gdp_series = gdp_local.loc[columns,:]
+
+    gdp_series = gdp_series.T
 
     if len(gdp_series) > 5:
         gdp_series.index = gdp_series.index.str.split("-").str[0]
