b003254c78b6fc73681d14c230dc3334ef25eab8
Andres Rios <riosandres294@gmail.com>
2026-03-31 23:03:38 -0500

    gdp section finished

--- pages/helpers/macro/macro_charts.py
+++ pages/helpers/macro/macro_charts.py
@@ -1,8 +1,10 @@
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+from pages.helpers.macro import macro_functions as mf
 
 def total_gdp_line(data: pd.DataFrame, labels: dict):
-    fig = px.line(data, labels={"index": "Year", "value": "Billions (COP)"}, title="GDP per Year" )
+    fig = px.line(data, labels={"index": "Year", "value": "Billions (COP)"})
     fig.update_layout(
 
         height=600,
@@ -11,11 +13,9 @@ def total_gdp_line(data: pd.DataFrame, labels: dict):
             "text": "GDP per Year",
             "font": {"size": 25}, 
             "x": 0,            
-            "xanchor": 'left'
+            "xanchor": "left"
         },
-        
-        #showlegend=False,
-        
+         
         margin=dict(l=50, r=20, t=80, b=0),
     )
 
@@ -59,3 +59,87 @@ def total_gdp_line(data: pd.DataFrame, labels: dict):
     )
     return fig
 
+def gdp_growth(df: pd.DataFrame, year: list, presiden: str, index: int, quarter: str|None):
+    df, df_local = mf.clean_annual_growth(df, year, presiden, index, quarter)
+    if len(df_local) > 1:
+        fig = px.line(df_local, labels={"value": "Growth (%)", "Fecha": "Year"})
+
+        fig.update_layout(
+
+            height=600,
+
+            title={
+                "text": "Real Annual GDP Growth",
+                "font": {"size": 25}, 
+                "x": 0,            
+                "xanchor": "left"
+            },
+            
+            showlegend=False,
+            
+            margin=dict(l=50, r=20, t=80, b=0),
+        )
+
+        fig.update_yaxes(
+            showgrid=True, 
+            gridwidth=0.5, 
+            gridcolor="rgba(255, 255, 255, 0.1)",
+            tickfont=dict(size=15),
+            tickformat=",.0f"
+        )
+
+        fig.update_xaxes(
+            type="category",
+            dtick=1,            
+            tickangle=45,       
+            showgrid=False,
+            tickfont=dict(size=15)
+        )
+
+        fig.update_layout(
+            xaxis_title_font=dict(size=15),
+            yaxis_title_font=dict(size=15),
+        )
+
+        fig.for_each_trace(lambda t: t.update(
+            hovertemplate="<br>Year: %{x}<br>Growth (%): %{y:.2f}<extra></extra>"
+        ))
+
+    else:
+        column = df_local.columns[index - 1] 
+        growth = df_local[column].iloc[0]
+        min_growth = df[column].min()
+        max_growth = df[column].max()
+        avg_growth = df[column].median()
+
+        fig = go.Figure(go.Indicator(
+            mode = "gauge+number+delta",
+            value = growth,
+            number = {"valueformat": ".2f", "suffix": "%"}, 
+            delta = {
+                "reference": avg_growth, 
+                "position": "bottom",
+                "valueformat": ".2f",
+                "suffix": " vs Median" 
+            },
+            title = {"text": f"<b>{year[0]} GDP Growth</b>", "font": {"size": 24}},
+            gauge = {
+                "axis": {
+                    "range": [min_growth, max_growth],
+                    "tickformat": ".1f" 
+                },
+                "bar": {"color": "darkblue"},
+                "steps": [
+                    {"range": [min_growth, avg_growth], "color": "lightgray"},
+                    {"range": [avg_growth, max_growth], "color": "#e5f5e0"} 
+                ],
+                "threshold": {
+                    "line": {"color": "red", "width": 4},
+                    "thickness": 0.75,
+                    "value": avg_growth 
+                }
+            }
+        ))
+
+        fig.update_layout(margin=dict(t=80, b=20, l=30, r=30), height=400)    
+    return fig
