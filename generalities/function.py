25b8757b15b9b70cdb276c64a7d89b162642d835
Andres Rios <riosandres294@gmail.com>
2026-05-11 15:16:30 -0500

    first section of CPI finished, plus some general improvements

--- generalities/function.py
+++ generalities/function.py
@@ -1,8 +1,10 @@
-from generalities.presidents import presidents
-
+from generalities.dictionaries import presidents
 
 def get_valid_presidents(tmp_years) -> list:
     return [
         name for name, pres_years in presidents.items()
         if not set(pres_years).isdisjoint(tmp_years)
     ]
+
+def find_key_by_value(d, value):
+    return next((k for k, v in d.items() if v == value), None)
