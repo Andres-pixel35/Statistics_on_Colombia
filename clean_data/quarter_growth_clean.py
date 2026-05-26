de9fcdbfeb5c3a25aa46804412d3608686455bbf
Andres Rios <riosandres294@gmail.com>
2026-05-26 09:36:30 -0500

    filter per quarter added to total values in gdp and data updated

--- clean_data/quarter_growth_clean.py
+++ clean_data/quarter_growth_clean.py
@@ -1,23 +1,21 @@
 import pandas as pd
 
-path = "./data/banco_republica/GDP/quarter_growth.csv"
+input_path = "../data/original/banco_republica/GDP/quarter_growth.xlsx"
+output_path = "../data/banco_republica/GDP/quarter_growth.csv"
 
-df = pd.read_csv(path, encoding="utf-8", dtype=str, na_values="-")
+df = pd.read_excel(input_path, dtype=str)
 
-df = df.apply(lambda col: col.str.replace(".", "", regex=False))
-df = df.apply(lambda col: col.str.replace(",", ".", regex=False))
+df = df.drop(index=df.index[0]).reset_index(drop=True)
 
-df = df.drop(columns=[df.columns[1]])
-df = df.set_index("Fecha")
+df = df.rename(columns={df.columns[1]: "Variación Trimestral Anual"})
+
+df[df.columns[1]] = df[df.columns[1]].str.replace(",", ".", regex=False)
+
 df = df.dropna()
 
-df.index = pd.to_datetime(df.index, dayfirst=True)
+df["Fecha"] = pd.to_datetime(df["Fecha"], dayfirst=True)
 
 roman_map = {1: "I", 2: "II", 3: "III", 4: "IV"}
+df["Fecha"] = [f"{date.year}-{roman_map[date.quarter]}" for date in df["Fecha"]]
 
-df.index = [f"{date.year}-{roman_map[date.quarter]}" for date in df.index]
-
-df.columns = ["Variación Trimestral Anual", "Variación Año Corrido"]
-
-df.to_csv(path, encoding="utf-8", index=True)
-
+df.to_csv(output_path, encoding="utf-8", index=False)
